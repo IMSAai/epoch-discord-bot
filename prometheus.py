0dd42bafb698bf0d2c8832187b7adb7561904131
Chairless-maybe <liamnelson0505@gmail.com>
2020-07-17 21:09:23 -0500

    Added 12 hour default time to silence command and changed the time input to a parameter passed on the command, measured in minutes.

--- prometheus.py
+++ prometheus.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import requests
 from datetime import datetime
+from datetime import timedelta
 import os
 
 prometheus = commands.Bot(command_prefix='do ')
@@ -12,41 +13,55 @@ async def on_ready():
     print('Prometheus bot active')
 
 
+
 @prometheus.command(pass_context=True, name='silence')
-async def silence(ctx):
+async def silence(ctx, time=720):
     author = ctx.author
 
-    # Simple function to retrieve the author of a message
+    # Simple function to ensure the author of a message is the same as the one
+    # who inputted the command
     def check(m):
         return author == m.author
+
+    # Converts the minutes argument into a datetime object
+    def datetime_from_minutes(minutes):
+        end_time = datetime.now() + timedelta(minutes=minutes)
+        return end_time
 
     # Check the server name and the user's role to ensure that no one who isn't
     # allowed to use the bot is using it
     if ctx.guild == 'Epoch' and ('mother board' or 'cluster team' or 'post-board' in [y.name.lower for y in author.roles]):
 
-        # Implements a check to ensure that if a user does not respond, the bot will exit the command
         while True:
-            await ctx.send('Please specify an end time of the form\n[YYYY]-[MM]-[DD]T[hour]:[minute]:[second]Z')
-
-            # The bot will wait until the user inputs a date and time, for a maximum of 30 seconds
-
             try:
-                end_time = await prometheus.wait_for('message', check=check, timeout=30)
-            except TimeoutError: # If no message is entered within 30 seconds, a TimeoutError happens
-                await ctx.send('No valid time was entered within 30 seconds.')
-                break
+                time = int(time)
+            except TypeError:
+                ctx.send('Please enter a valid time')
+                # line to enter a new time value if the time entered is not an integer
+                try:
+                    time = await prometheus.wait_for('message', check=check, timeout=15)
+                except TimeoutError:
+                    continue
+            break
 
+        # Implements a check to ensure that if a user does not respond, the bot
+        # will exit the command
+        while True:
+            end_time = str(datetime_from_minutes(time)).replace(' ', 'T') + 'Z'
             await ctx.send('Any comment?')
 
             try:
-                comment = await prometheus.wait_for('message', check=check)
-            except TimeoutError:
-                await ctx.send('No valid time was entered within 30 seconds.')
-                break
+                comment = await prometheus.wait_for('message', check=check, timeout=45)
+                if comment.content.lower == 'no' or 'nope' or 'n':
+                    comment = 'No comment'
 
-            if comment.content.lower == 'no' or 'nope' or 'n':
+            except TimeoutError:
+                # defaults to no comment without a comment being entered
+                await ctx.send('No comment was entered within 45 seconds. Setting to default.')
                 comment = 'No comment'
-            # Assigns a message signifying the creation of the silence to a variable for later use
+
+            # Assigns a message signifying the creation of the silence to a
+            # variable for later use
             creating = await ctx.send('Creating silence for {}...'.format(author.name))
 
             now = str(datetime.now()).replace(' ', 'T') + 'Z'
@@ -68,7 +83,8 @@ async def silence(ctx):
             # ends the command
             break
 
-    # if a user does not meet the specified criteria, they will be unable to execute the command
+    # if a user does not meet the specified criteria, they will be unable to
+    # execute the command
     else:
         await ctx.send('You do not have permission to do that')
 
