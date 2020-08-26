572f8da044f92a7e0e5991be25ccc830fb631baf
Dev Singh <dev@devksingh.com>
2020-08-26 18:13:24 -0500

    add user assoc db

--- prometheus.py
+++ prometheus.py
@@ -3,7 +3,8 @@ from discord.ext import commands
 import requests
 from datetime import datetime
 from datetime import timedelta
-import os
+import sys
+import sqlite3
 
 prometheus = commands.Bot(command_prefix='do ')
 
@@ -15,9 +16,9 @@ async def on_ready():
 
 
 @prometheus.command(pass_context=True, name='silence')
+@commands.has_role("Cluster Team")
 async def silence(ctx, time=720):
     author = ctx.author
-
     # Simple function to ensure the author of a message is the same as the one
     # who inputted the command
     def check(m):
@@ -30,63 +31,87 @@ async def silence(ctx, time=720):
 
     # Check the server name and the user's role to ensure that no one who isn't
     # allowed to use the bot is using it
-    if ctx.guild == 'Epoch' and ('mother board' or 'cluster team' or 'post-board' in [y.name.lower for y in author.roles]):
-
-        while True:
+    while True:
+        try:
+            time = int(time)
+        except TypeError:
+            ctx.send('Please enter a valid time')
+            # line to enter a new time value if the time entered is not an integer
             try:
-                time = int(time)
-            except TypeError:
-                ctx.send('Please enter a valid time')
-                # line to enter a new time value if the time entered is not an integer
-                try:
-                    time = await prometheus.wait_for('message', check=check, timeout=15)
-                except TimeoutError:
-                    continue
+                time = await prometheus.wait_for('message', check=check, timeout=15)
+            except TimeoutError:
+                continue
+        break
+
+    # Implements a check to ensure that if a user does not respond, the bot
+    # will exit the command
+    while True:
+        end_time = str(datetime_from_minutes(time)).replace(' ', 'T') + 'Z'
+        await ctx.send('Any comment?')
+        try:
+            comment = await prometheus.wait_for('message', check=check, timeout=45)
+            if comment.content.lower == 'no' or 'nope' or 'n':
+                comment = 'No comment'
+        except TimeoutError:
+            # defaults to no comment without a comment being entered
+            await ctx.send('No comment was entered within 45 seconds. Cancelling...')
             break
-
-        # Implements a check to ensure that if a user does not respond, the bot
-        # will exit the command
-        while True:
-            end_time = str(datetime_from_minutes(time)).replace(' ', 'T') + 'Z'
-            await ctx.send('Any comment?')
-
+        conn = sqlite3.connect('userassoc.db')
+        cursor = conn.execute('''
+            SELECT * FROM USERDATA
+            WHERE discorduser="{}"
+        '''.format(author))
+        records = cursor.fetchone()
+        conn.close()
+        name, email = records[1] if records != [] else "", records[2] if records != [] else ""
+        if records == []:
+            await ctx.send('We don\'t seem to have your real name on file. What is your real name?')
             try:
-                comment = await prometheus.wait_for('message', check=check, timeout=45)
-                if comment.content.lower == 'no' or 'nope' or 'n':
-                    comment = 'No comment'
-
+                name = await prometheus.wait_for('message', check=check, timeout=45).content
             except TimeoutError:
                 # defaults to no comment without a comment being entered
-                await ctx.send('No comment was entered within 45 seconds. Setting to default.')
-                comment = 'No comment'
+                await ctx.send('No name was entered within 45 seconds. Cancelling...')
+                break
+            await ctx.send('What is your IMSA email?')
+            try:
+                email = await prometheus.wait_for('message', check=check, timeout=45).content
+            except TimeoutError:
+                # defaults to no comment without a comment being entered
+                await ctx.send('No name was entered within 45 seconds. Cancelling...')
+                break
+            query = "REPLACE INTO USERDATA (discorduser,name,email) VALUES ('{}', '{}', '{}')".format(author, name, email)
+            print(query)
+            conn.execute(query)
+            conn.commit()
+            conn.close()
+        else:
+            pass
+        # Assigns a message signifying the creation of the silence to a
+        # variable for later use
+        creating = await ctx.send('Creating silence for {} <{}>...'.format(name, email))
 
-            # Assigns a message signifying the creation of the silence to a
-            # variable for later use
-            creating = await ctx.send('Creating silence for {}...'.format(author.name))
-
-            now = str(datetime.now()).replace(' ', 'T') + 'Z'
-            url = 'http://alerts.epoch.imsa.edu/api/v1/silences'
-            post_data = {"id": "",
-                         "createdBy": author.name,
-                         "comment": comment,
-                         "startsAt": now,
-                         "endsAt": end_time,
-                         "matchers": [{"name": "job",
-                                       "value": "evc_slurm_exporter",
-                                       "isRegex": False}]}
+        now = str(datetime.now()).replace(' ', 'T') + 'Z'
+        url = 'http://alerts.epoch.imsa.edu/api/v1/silences'
+        post_data = {"id": "",
+                        "createdBy": "{} {}".format(name, email),
+                        "comment": comment,
+                        "startsAt": now,
+                        "endsAt": end_time,
+                        "matchers": [{"name": "job",
+                                    "value": "evc_slurm_exporter",
+                                    "isRegex": False}]}
+        try:
             x = requests.post(url, data=post_data)
-
-            # edits the previous message to show that the request has gone through
-            await creating.edit(content='Creating silence for{}...done'.format(author.name))
-            # sends the response to the Discord channel
-            await ctx.send(x)
-            # ends the command
+        except: 
+            await ctx.send("Error silencing the alert. Please try again.")
             break
 
-    # if a user does not meet the specified criteria, they will be unable to
-    # execute the command
-    else:
-        await ctx.send('You do not have permission to do that')
+        # edits the previous message to show that the request has gone through
+        await creating.edit(content='Creating silence for {} <{}> ... done!'.format(name, email))
+        # sends the response to the Discord channel
+        await ctx.send(x)
+        # ends the command
+        break
 
 
 @prometheus.command()
@@ -94,6 +119,6 @@ async def silence(ctx, time=720):
 async def ping(ctx):
     await ctx.send('latency: {} ms'.format(round(prometheus.latency * 1000)))
 
-key = os.environ['DISCORD_KEY']
+key = sys.argv[1]
 
 prometheus.run(key)
